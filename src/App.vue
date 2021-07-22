35142c3db33a1534211c8ad21448d0eb5e81fb3c
Miguel Angel Caro Garcia <miguel@factorial.io>
2021-07-22 12:47:06 +0200

    First calendar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Kalendar :configuration="calendar_settings" :events.sync="events" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Kalendar } from 'kalendar-vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Kalendar
+  },
+  data: () => ({
+    calendar_settings: {
+      style: 'material_design',
+      view_type: 'week',
+      cell_height: 20,
+      scrollToNow: true,
+      start_day: new Date().toISOString(),
+      read_only: false,
+      day_starts_at: 0,
+      day_ends_at: 24,
+      overlap: true,
+      hide_dates: ['2019-10-31'], // Spooky
+      hide_days: [6],
+      past_event_creation: true
+    },
+    events: [
+      {
+        from: '2020-03-18T18:00:00Z',
+        to: '2020-03-18T19:00:00Z',
+        data: 'Event 1',
+      },
+      {
+        from: '2020-03-18T19:00:00Z',
+        to: '2020-03-18T21:00:00Z',
+        data: 'Olive & Friends',
+      },
+    ],
+  })
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+import PortalVue from 'portal-vue';
+
+import { Kalendar } from 'kalendar-vue';
+
+Vue.use(PortalVue);
+Vue.use(Kalendar);
 
 Vue.config.productionTip = false
 
