2864ad5845f940f7a8cecc62eac9f328c8cea9cb
Miguel Angel Caro Garcia <miguelangel.caro@taiger.com>
2021-10-17 17:56:51 +0200

    deploy

---
+++ precache-manifest.03d661b68dfd46d38c0648cdfe8e7a79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e8434d1edb3fafae94",
+    "url": "/swimming-book/css/app.601d2ada.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/swimming-book/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f5d704402008a5adc8626e98f5279544",
+    "url": "/swimming-book/index.html"
+  },
+  {
+    "revision": "26e8434d1edb3fafae94",
+    "url": "/swimming-book/js/app.d11ffb90.js"
+  },
+  {
+    "revision": "87d9d4b8145fd987fa1e",
+    "url": "/swimming-book/js/chunk-vendors.9d184245.js"
+  },
+  {
+    "revision": "cee78f1d8becf8688df1961d83795214",
+    "url": "/swimming-book/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/swimming-book/robots.txt"
+  }
+]);
